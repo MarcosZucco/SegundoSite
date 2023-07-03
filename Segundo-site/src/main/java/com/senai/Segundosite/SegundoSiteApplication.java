5b9032fdc6cc8d4b8fbd8677334f54c49aef56a4
Marcos Zucco <marcosdanilofernandes.pd1@gmail.com>
2023-07-02 21:22:58 -0300

    SegundoSite

---
+++ Segundo-site/src/main/java/com/senai/Segundosite/SegundoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.senai.Segundosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoSiteApplication.class, args);
+	}
+
+}
---
+++ Segundo-site/src/test/java/com/senai/Segundosite/SegundoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.Segundosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
